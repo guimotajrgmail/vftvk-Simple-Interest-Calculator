9d7ca99143ef7b0849abe908ed19755b4f085fb5
Guilherme Mota <guimotajr@gmail.com>
2021-07-08 20:26:57 +0000

    Alteração inclusão br html. alteração script

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ function compute()
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
     
-    document.getElementById("result").innerHTML= "If you deposit" + principal + ", \<br\>at an interest rate of " + 
-    rate + "\<br\> You will receive an amount of" +interest+ ", \<br\>in the year "+year+"\<br\>"
+    <!-- Calculo para resultado final-->
+    document.getElementById("result").innerHTML= "\<br\>If you deposit " + "\<mark>" principal "\</mark>" + ", \<br\>at an interest rate of " + 
+    "\<mark>" rate "\</mark>" + ".\<br\> You will receive an amount of " + "\<mark>" interest "\</mark>" + ", \<br\>in the year "+ "\<mark>" year "\</mark>" +"\<br\>"
 
     
 }
